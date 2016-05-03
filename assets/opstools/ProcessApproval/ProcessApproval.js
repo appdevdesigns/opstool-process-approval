13a415c935084aecac33475d102a9083f0ad4383
Pong <wongpratan@gmail.com>
2016-05-03 11:12:24 +0700

    Fix build/minify to production

--- assets/opstools/ProcessApproval/ProcessApproval.js
+++ assets/opstools/ProcessApproval/ProcessApproval.js
@@ -1,7 +1,6 @@
 steal(
 // List your Page's dependencies here:
 	'opstools/ProcessApproval/ProcessApproval.css',
-	'site/labels/opstool-ProcessApproval.js',
 	function () {
 		steal.import('opstools/ProcessApproval/controllers/ProcessApproval').then(function () { });
 	});
